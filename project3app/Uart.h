79b36770b6f2a4fd5964be15c707d12a2f497608
OmarKazem <114486423+OmarKazem@users.noreply.github.com>
2023-12-06 23:26:14 +0200

    Update Uart.h

--- project3app/Uart.h
+++ project3app/Uart.h
@@ -7,14 +7,7 @@ void Uart_SetBaudRate(unsigned short BuadRate);
 
 void Uart_SendChar(unsigned char DataByte);
 
-unsigned char Uart_IsDataAvailable(void);
-
-unsigned char Uart_ReadData();
-
 void Uart_SendString(char DataString[], unsigned char Size);
 
-void Uart_ReadString(char DataString[], unsigned char Size);
-
-unsigned char Uart_ReadNumber(unsigned char Size);
 
 #endif
