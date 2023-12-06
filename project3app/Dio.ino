b104c7f58b96371d6f9a67e3a3eb9f9672d13d50
OmarKazem <114486423+OmarKazem@users.noreply.github.com>
2023-12-06 23:25:12 +0200

    Update Dio.ino

--- project3app/Dio.ino
+++ project3app/Dio.ino
@@ -1,50 +1,81 @@
+#include <stdio.h>
+#include <util/delay.h>
 #include "Dio.h"
 
+
 void DIO_init(){
+  //set pins PD7 as output
+  DIO_SetPinDirection('b', 5, 'o');
+  DIO_SetPinDirection('b', 4, 'o');
+}
+
+// set Pin to INPUT or OUTput
+void DIO_SetPinDirection(unsigned char port, int pin, unsigned char direction){ //
+  if (port == 'b' & pin <= 7 & pin >= 0){ // pin must be from 0-7
+    if(direction == 'o'){
+      DDRB = DDRB|(1<<pin); //bit set to 1 for output
+    }else if(direction == 'i'){
+      DDRB = DDRB&!(1<<pin); // bit cleared to 0 for input
+    }
+  }
+  if (port == 'c' & pin <= 7 & pin >= 0){ // pin must be from 0-7
+    if(direction == 'o'){
+      DDRC = DDRC|(1<<pin); // bit set to 1 for output
+    }else if(direction == 'i'){
+      DDRC = DDRC&!(1<<pin); // bit cleared to 0 for input
+    }
+  }
+  if (port == 'd' & pin <= 7 & pin >= 0){ // pin must be from 0-7
+    if(direction == 'o'){
+      DDRD = DDRD|(1<<pin); //bit set to 1 for output
+    }else if(direction == 'i'){
+      DDRD = DDRD&!(1<<pin); //bit cleared to 0 for input
+    }
+  }
+}
+
+// if pin is output it will set it to either HIGH or LOW
+// if pin is input it will activate internal pull up resistor
+void DIO_SetPinState(unsigned char port, int pin, unsigned char state){
+  if (port == 'b' & pin <= 7 & pin >= 0){ // pin must be from 0-7
+    if(state == 'h'){
+      PORTB = PORTB|(1<<pin); //pin is set to 1 for high
+    }else if(state == 'l'){
+      PORTB &= ~(1<<pin); //pin is cleared to 0 for low
+    }
+  }
+  if (port == 'd' & pin <= 7 & pin >= 0){ // pin must be from 0-7
+    if(state == 'h'){
+      PORTD = PORTD|(1<<pin); //pin is set to 1 for high
+    }else if(state == 'l'){
+      PORTD &= ~(1<<pin); //pin is cleared to 0 for low
+    }
+  }
+  if (port == 'c' & pin <= 7 & pin >= 0){ // pin must be from 0-7
+    if(state == 'h'){
+      PORTC = PORTC|(1<<pin); //pin is set to 1 for high
+    }else if(state == 'l'){
+      PORTC &= ~(1<<pin); //pin is cleared to 0 for low
+    }
+  }
+}
   
-}
 
-
-inline void Dio_SetRegisterBit(volatile unsigned char* Register, unsigned char Bit){
-  *Register = *Register | (1 << Bit);
-}
-
-inline void Dio_ResetRegisterBit(volatile unsigned char* Register, unsigned char Bit){
-  *Register = *Register & ~(1 << Bit);
-}
-
-inline void Dio_ToggleRegisterBit(volatile unsigned char* Register, unsigned char Bit){
-  *Register = *Register ^ (1 << Bit);
-}
-
-inline unsigned char Dio_GetRegisterBit(volatile unsigned char* Register, unsigned char Bit){
-  return (*Register >> Bit) & (unsigned char)0x01;
-}
-
-void Dio_SetPinDirection(volatile unsigned char* Register, unsigned char Pin, unsigned char Direction) {
-    if (Direction == DIO_DIR_INPUT)
-    {
-      Dio_ResetRegisterBit(Register, Pin);
-    } else{
-      Dio_SetRegisterBit(Register, Pin);
-    }
-}
-
-void Dio_SetPinState(volatile unsigned char* Register, unsigned char Pin, unsigned char State) {
-    if (State == DIO_STATE_LOW)
-    {
-      Dio_ResetRegisterBit(Register, Pin);
-    } else{
-      Dio_SetRegisterBit(Register, Pin);
-    }
-}
-
-unsigned char Dio_GetPinState(volatile unsigned char* Register, unsigned char Pin) {
-  return Dio_GetRegisterBit(Register, Pin);
-}
-
-
-int readPinState(int pin) {
-      int MS = ((PINC >> pin) & 1) == 0;
+int readPinState(unsigned char port,int pin) {
+  if (port == 'c' & pin <= 7 & pin >= 0){
+      int MS = ((PINC >> pin) & 1) == 1;
       return MS;
       }
+
+  else if (port == 'd' & pin <= 7 & pin >= 0){
+    int MS = ((PIND >> pin) & 1) == 1;
+      return MS;
+      }
+
+}
+
+
+
+
+
+
