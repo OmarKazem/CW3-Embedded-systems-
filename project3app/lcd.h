cefe515b1701dff9c9a086640838540dc266afeb
OmarKazem <114486423+OmarKazem@users.noreply.github.com>
2023-12-06 23:27:05 +0200

    Update lcd.h

--- project3app/lcd.h
+++ project3app/lcd.h
@@ -5,7 +5,6 @@ void LCD_Command(unsigned char cmnd);
 void LCD_Char (unsigned char char_data);
 void LCD_Init (void);
 void LCD_String (char *str);
-void LCD_String_xy (char row, char pos, char *str);
-void LCD_Clear();
+
 
 #endif /*__LCD__*/
