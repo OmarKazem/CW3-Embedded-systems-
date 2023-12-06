a44068406a4cc1a7a5bd72f8446e0f73f62d398e
OmarKazem <114486423+OmarKazem@users.noreply.github.com>
2023-12-06 23:46:09 +0200

    Update project3app.ino

--- project3app/project3app.ino
+++ project3app/project3app.ino
@@ -7,10 +7,14 @@
 #include "pwm.h"
 #include "Uart.h"
 
-// Function prototypes and global variables declarations
+// Function to toggle the system power state
+void toggleSystemPower(unsigned char port,int buttonPin, int *powerState) {
+    // Toggle the power state when the button is pressed
+    *powerState = (*powerState == HIGH) ? LOW : HIGH;
+}
+
 
 int main(void) {
-    
     unsigned short adc_reading;
     unsigned char buffer[4];    // Buffer for converting numerical values to strings
     unsigned char buffer2[4];   // Another buffer for numerical to string conversion
@@ -21,7 +25,8 @@ int main(void) {
     int SetTemp;                 // Variable to store the set temperature
     int address = 0;             // EEPROM address for storing set temperature
     int speed = 190;             // Default speed value
-    
+    int powerState = LOW;        // Initial power state
+
     while (1) {
         // Read the set temperature from EEPROM
         SetTemp = EEPROM.read(address);
@@ -29,19 +34,26 @@ int main(void) {
         // Read temperature from ADC
         adc_reading = Adc_ReadChannel(1);
         float temperatureCelsius = ((adc_reading / 1024.0) * 5 * 100.0);
-        
+
         // Convert numerical values to strings for LCD display and UART transmission
         intarg(temperatureCelsius, buffer, 10);
         intarg(SetTemp, buffer2, 10);
-        
-        // Read state of switches
-        int RPS2  = readPinState('c',2);
-        int RPS3  = readPinState('c',3);
-        int RPS4  = readPinState('c',4);
-        int RPS5  = readPinState('d',2);
 
-        
-        
+        // Read state of switches
+        int RPS2  = readPinState('c', 2);
+        int RPS3  = readPinState('c', 3);
+        int RPS4  = readPinState('c', 4);
+        int RPS5  = readPinState('d', 2);
+
+        // Read state of the power button
+        int powerButtonState = readPinState('c', 5);
+
+        // Toggle the system power if the power button is pressed
+        if (powerButtonState == HIGH) {
+            toggleSystemPower('c', 5, &powerState);
+            _delay_ms(200);  // Debounce delay
+        }
+
         // Display temperature and set temperature on LCD
         LCD_Command(0x80);
         LCD_String("Temp: ");
@@ -51,7 +63,7 @@ int main(void) {
         LCD_String(buffer2);
 
         // Check if the "Increase SetTemp" button is pressed
-        if (RPS2 == 0) {
+        if (RPS2 == HIGH) {
             ++SetTemp;
             EEPROM.write(address, SetTemp);  // Write the updated set temperature to EEPROM
             Uart_SendString("Target Increased", 16);
@@ -59,7 +71,7 @@ int main(void) {
             _delay_ms(200);
         } 
         // Check if the "Decrease SetTemp" button is pressed
-        else if (RPS3 == 0) {
+        else if (RPS3 == HIGH) {
             --SetTemp;
             EEPROM.write(address, SetTemp);  // Write the updated set temperature to EEPROM
             Uart_SendString("Target Decreased", 16);
@@ -68,37 +80,49 @@ int main(void) {
         }
 
         // Check if the "Increase Speed" button is pressed
-        if (RPS4 == 0) {
+        if (RPS4 == HIGH) {
             speed = 255;
             Uart_SendString("Speed increased  \n", 16);
             Uart_SendString("\n", 2);
         }
         // Check if the "Decrease Speed" button is pressed
-        else if (RPS5 == 0) {
+        else if (RPS5 == HIGH) {
             speed = 190;
             Uart_SendString("Speed decreased  \n", 16);
             Uart_SendString("\n", 2);
         }
 
         // Control the heating/cooling system based on temperature and set temperature
-        if (temperatureCelsius > SetTemp) {
-            // Cooling mode
-            Set_DutyCycle(0, 3);
-            Set_DutyCycle(speed, 11);
-            DIO_SetPinState('b', 4, 'l');
-            DIO_SetPinState('b', 5, 'h');
-            Uart_SendString("System cooling", 16);
-            Uart_SendString("\n", 2);
+        if (powerState == HIGH) {
+            if (temperatureCelsius > SetTemp) {
+                // Cooling mode
+                Set_DutyCycle(0, 3);
+                Set_DutyCycle(speed, 11);
+                DIO_SetPinState('b', 4, 'l');
+                DIO_SetPinState('b', 5, 'h');
+                Uart_SendString("System cooling", 16);
+                Uart_SendString("\n", 2);
+            } else {
+                // Heating mode
+                Set_DutyCycle(speed, 3);
+                Set_DutyCycle(0, 11);
+                DIO_SetPinState('b', 5, 'l');
+                DIO_SetPinState('b', 4, 'h');
+                Uart_SendString("System heating", 16);
+                Uart_SendString("\n", 2);
+            }
         } else {
-            // Heating mode
-            Set_DutyCycle(speed, 3);
+            // System is turned off
+            Set_DutyCycle(0, 3);
             Set_DutyCycle(0, 11);
-            DIO_SetPinState('b', 5, 'l');
-            DIO_SetPinState('b', 4, 'h');
-            Uart_SendString("System heating", 16);
+            Uart_SendString("System turned off", 16);
             Uart_SendString("\n", 2);
+            DIO_SetPinState('b', 5, 'l');
+            DIO_SetPinState('b', 4, 'l');
         }
     } /* End event loop */
 
     return 0;
 }
+
+
